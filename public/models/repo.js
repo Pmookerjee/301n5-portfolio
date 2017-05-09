cad98a984a6bb6753bb0efc1120b4d4719052b09
Pmookerjee <pmookerjee@gmail.com>
2017-05-09 09:00:36 -0700

    added a console log

--- public/models/repo.js
+++ public/models/repo.js
@@ -11,20 +11,6 @@
     $.get('/github/user/repos')
     .then(data => repos.all = data, err => console.error(err))
     .then(callback);
-  //   $.get('https://api.github.com/user?access_token=')
-  //     .then(user => {
-  //       $.get(user.repos_url)
-  //         .then(results => {
-  //           results.forEach(repo => {
-  //             repos.all.push(repo);
-  //           })
-  //           callback(repos);
-  //         }, error => {
-  //           console.log(error);
-  //         });
-  //     }, error => {
-  //       console.log(error);
-  //     });
   };
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
--- public/views/repoView.js
+++ public/views/repoView.js
@@ -3,16 +3,16 @@
 (function(module) {
   const repoView = {};
 
-  // const ui = function() {
-  //   $('ul.repo_ul').empty();
-  // };
+  const ui = function() {
+    $('ul.repo_ul').empty();
+  };
 
   var source   = $("#repo-template").text();
   var render = Handlebars.compile(source);
 
   repoView.index = function() {
-    // ui();
-    console.log(repos);
+    ui();
+    console.log('in the repoView.index()');
     repos.with('name').map(render);
   };
 
