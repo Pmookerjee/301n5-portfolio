c6b7c0102178e8496915afebbf233151e3a596e8
Pmookerjee <pmookerjee@gmail.com>
2017-05-09 00:30:19 -0700

    experimenting romoved .empty method

--- public/views/repoView.js
+++ public/views/repoView.js
@@ -3,15 +3,15 @@
 (function(module) {
   const repoView = {};
 
-  const ui = function() {
-    $('ul.repo_ul').empty();
-  };
+  // const ui = function() {
+  //   $('ul.repo_ul').empty();
+  // };
 
   var source   = $("#repo-template").text();
   var render = Handlebars.compile(source);
 
   repoView.index = function() {
-    ui();
+    // ui();
     console.log(repos);
     repos.with('name').map(render);
   };
