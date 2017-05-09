7b7abd47284023265d3c45def36e76a7b02286ef
Pmookerjee <pmookerjee@gmail.com>
2017-05-09 08:56:10 -0700

    nope, forgot that wont work for heroku

--- public/models/repo.js
+++ public/models/repo.js
@@ -8,24 +8,24 @@
 
   repos.requestRepos = function(callback) {
 
-    // $.get('/github/user/repos')
-    // .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
-    // .then(callback);
-    $.get('https://api.github.com/user?access_token=')
-      .then(user => {
-        $.get(user.repos_url)
-          .then(results => {
-            results.forEach(repo => {
-              repos.all.push(repo);
-            })
-            callback(repos);
-          }, error => {
-            console.log(error);
-          });
-      }, error => {
-        console.log(error);
-      });
-  };
+    $.get('/github/user/repos')
+    .then(data => repos.all = data, err => console.error(err))
+    .then(callback);
+  //   $.get('https://api.github.com/user?access_token=')
+  //     .then(user => {
+  //       $.get(user.repos_url)
+  //         .then(results => {
+  //           results.forEach(repo => {
+  //             repos.all.push(repo);
+  //           })
+  //           callback(repos);
+  //         }, error => {
+  //           console.log(error);
+  //         });
+  //     }, error => {
+  //       console.log(error);
+  //     });
+  // };
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
   module.repos = repos;
