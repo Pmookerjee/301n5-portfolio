24894df8d37b369313fa4f3b0e83600f4787b9ac
Pmookerjee <pmookerjee@gmail.com>
2017-05-09 08:54:04 -0700

    changed the ajax request to the non-proxy version

--- public/models/repo.js
+++ public/models/repo.js
@@ -8,23 +8,23 @@
 
   repos.requestRepos = function(callback) {
 
-    $.get('/github/user/repos')
-    .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
-    .then(callback);
-    // $.get('https://api.github.com/user?access_token=' + window.gitToken)
-    //   .then(user => {
-    //     $.get(user.repos_url + '?access_token=' + window.gitToken)
-    //       .then(results => {
-    //         results.forEach(repo => {
-    //           repos.all.push(repo);
-    //         })
-    callback(repos);
-      //     }, error => {
-      //       console.log(error);
-      //     });
-      // }, error => {
-      //   console.log(error);
-      // });
+    // $.get('/github/user/repos')
+    // .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
+    // .then(callback);
+    $.get('https://api.github.com/user?access_token=')
+      .then(user => {
+        $.get(user.repos_url)
+          .then(results => {
+            results.forEach(repo => {
+              repos.all.push(repo);
+            })
+            callback(repos);
+          }, error => {
+            console.log(error);
+          });
+      }, error => {
+        console.log(error);
+      });
   };
   repos.with = attr => repos.all.filter(repo => repo[attr]);
 
